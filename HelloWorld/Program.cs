98d35c5b252df09326862d9cdb14fb66ec73da48
Jesper Engdahl <p3pp33ng@gmail.com>
2017-10-30 16:25:51 +0100

    Fixed console writelines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,14 +15,6 @@ namespace HelloWorld
             Console.WriteLine("Fredrik was here!");
             Console.WriteLine("Patrik too");
             Console.WriteLine(jesper.Speak());
-        }
-
-        internal class Jesper
-        {
-            public string Speak()
-            {
-                return "Jesper was here";
-            }
             Console.WriteLine("Isabella");
             Console.WriteLine("Fredrik was here!");
             Console.WriteLine("Erik Jonson was here");
@@ -30,6 +22,14 @@ namespace HelloWorld
             Console.WriteLine("Andreas was also here omg!!");
             Console.WriteLine("Nu blir det en mergekonflikt - som jag löst!");
             Console.WriteLine("Patrik too");
+        }
+
+        internal class Jesper
+        {
+            public string Speak()
+            {
+                return "Jesper was here";
+            }            
         }
     }
 }
