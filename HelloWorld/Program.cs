16e4f40e8b9880233a1de0a290ece4df96cd8162
Fredrik Haglund <mejl@fredrikhaglund.se>
2017-10-30 16:09:01 +0100

    Fixat med utropstecken...

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Fredrik was here!!!");
+            Console.WriteLine("Fredrik was here!");
         }
     }
 }
